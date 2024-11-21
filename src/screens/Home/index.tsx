04ed4abb25b88e42e4179d8ad50cf22c35dd3923
LanDev01 <alnjr0001@outlook.com>
2024-11-21 15:54:07 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,33 @@
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { styles } from "./styles"
+
+import { Participant } from '../../components/participant';
+
+export function Home() {
+  function handleParticipantAdd() {
+    console.log('Clicou');
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de novembro de 2022</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder='Nome do participante'
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Participant />
+      <Participant />
+      <Participant />
+    </View>
+  );
+}
